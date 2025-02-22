c8ac1e7860a1d8a812921d7de01941a7705766c0
Tony Hayes <majick777@gmail.com>
2025-02-22 23:05:55 +1000

    2.5.9.9 fixes

--- loader.php
+++ loader.php
@@ -5,7 +5,7 @@
 // =================================
 
 // -------------
-// Loader v1.3.4
+// Loader v1.3.5
 // -------------
 // Note: Changelog at end of file.
 
@@ -1992,7 +1992,7 @@ if ( !class_exists( 'stream_player_loader' ) ) {
 			echo '</div>' . "\n";
 
 			// 1.3.6: move notice boxer scripts to setting_scripts
-			$this->scripts[] = 'notice_boxer';
+			// $this->scripts[] = 'notice_boxer';
 
 		}
 
@@ -2334,7 +2334,7 @@ if ( !class_exists( 'stream_player_loader' ) ) {
 				// --- output tab switcher script ---
 				// 1.0.9: add to settings scripts
 				// 1.2.5: only store script reference
-				$this->scripts[] = 'tab_switcher';
+				// $this->scripts[] = 'tab_switcher';
 
 				$i = 0;
 				echo '<ul id="settings-tab-buttons">' . "\n";
@@ -2355,7 +2355,7 @@ if ( !class_exists( 'stream_player_loader' ) ) {
 
 			// --- reset to default script ---
 			// 1.0.9: add to settings scripts
-			$this->scripts[] = 'settings_reset';
+			// $this->scripts[] = 'settings_reset';
 
 			// --- start settings form ---
 			// 1.2.0: remove unused prefix on settings tab name attribute
@@ -2544,6 +2544,9 @@ if ( !class_exists( 'stream_player_loader' ) ) {
 		// 1.2.3: added for standalone enqueueing of resources from table
 		// 1.2.4: added missing public visibility declaration
 		public function settings_resources( $media = true, $color_picker = true ) {
+
+			// 1.3.5
+			$this->scripts = array( 'notice_boxer', 'tab_switcher', 'settings_reset' );
 
 			// --- number input step script ---
 			// 1.0.9: added to script array
@@ -3241,10 +3244,7 @@ if ( !class_exists( 'stream_player_loader' ) ) {
 						echo "});" . "\n";
 
 					}
-					// else {
-						// [no longer implemented - no escape option]
-						// echo $script;
-					// }
+
 				}
 
 				// 1.2.5: added for possible extra settings scripts
