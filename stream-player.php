88f7cf8d50fd8076fd825bb5fa78699612b66034
Tony Zeoli <az@digitalstrategyworks.com>
2025-03-05 16:54:43 -0500

    Update version to 2.5.9.10
    Match w/ stable tag to 2.5.9.10

--- stream-player.php
+++ stream-player.php
@@ -6,7 +6,7 @@ Plugin Name: Stream Player
 Plugin URI: https://streamplayer.pro
 Description: Adds an advanced Streaming Audio Player your site.
 Author: Tony Hayes, Tony Zeoli
-Version: 2.5.9.9
+Version: 2.5.9.10
 License: GPLv2 or later
 Requires at least: 4.0
 Text Domain: stream-player
